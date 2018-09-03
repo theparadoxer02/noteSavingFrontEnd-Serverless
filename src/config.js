075da75e26b90995aba684132a88534c5d89ff16
Mannu Gupta <abhimanyu98986@gmail.com>
2018-09-03 20:01:31 +0530

    Configuring environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,44 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    REGION: "YOUR_DEV_S3_UPLOADS_BUCKET_REGION",
+    BUCKET: "YOUR_DEV_S3_UPLOADS_BUCKET_NAME"
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: "YOUR_DEV_API_GATEWAY_REGION",
+    URL: "YOUR_DEV_API_GATEWAY_URL"
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    REGION: "YOUR_DEV_COGNITO_REGION",
+    USER_POOL_ID: "YOUR_DEV_COGNITO_USER_POOL_ID",
+    APP_CLIENT_ID: "YOUR_DEV_COGNITO_APP_CLIENT_ID",
+    IDENTITY_POOL_ID: "YOUR_DEV_IDENTITY_POOL_ID"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "YOUR_PROD_S3_UPLOADS_BUCKET_REGION",
+    BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
+  },
+  apiGateway: {
+    REGION: "YOUR_PROD_API_GATEWAY_REGION",
+    URL: "YOUR_PROD_API_GATEWAY_URL"
+  },
+  cognito: {
+    REGION: "YOUR_PROD_COGNITO_REGION",
+    USER_POOL_ID: "YOUR_PROD_COGNITO_USER_POOL_ID",
+    APP_CLIENT_ID: "YOUR_PROD_COGNITO_APP_CLIENT_ID",
+    IDENTITY_POOL_ID: "YOUR_PROD_IDENTITY_POOL_ID"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
